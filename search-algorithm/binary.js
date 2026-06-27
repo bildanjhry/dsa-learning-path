10fa244dc3e737df9400dc83234337bc6fd2d440
bildan <bildanjauhary@gmail.com>
2026-06-28 01:21:48 +0700

    feat: add search algorithm -> binary search implementation into dsa learning path

---
+++ search-algorithm/binary.js
@@ -0,0 +1,58 @@
+// Binary Search
+
+// jika kita ingin mencari angka dan menggunakan linear search
+// lalu menggunakan konsep 2 pointer sehingga secara time complexity = O(n) dan auxiliary space = O(1)
+// namun jika n = 10000000 maka fungsi ini menjadi f(n) = 5000000n + 3
+function findIndex(arr, target){
+    let left = 0
+    let right = arr.length-1
+    while(left <= right){
+        if(arr[left] === target) return left
+        else if(arr[right] === target) return right
+        left++
+        right--
+    }
+    return -1
+}
+
+console.log(findIndex([12, 22, 8, 10, 7, 5], 10))
+
+// menggunakan perulangan 1 arah
+function findIndexOneLine(arr, target){
+    let i = 0
+    while(i <= arr.length-1){
+        if(arr[i] === target) return i
+        i++
+    }
+    return -1
+}
+
+// dengan menggunakan recursive function konsep 2 pointer
+function findIndexRec(arr, target, curr){
+    if(curr === 0) return -1
+    let left = 0
+    if(arr[left] === target) return left
+    else if(arr[curr] === target) return curr
+    left++
+    findIndexRec(arr, target, curr-1)
+}
+
+const arr = [12, 11, 8, 10, 7, 2]
+console.log(findIndex(arr, 2, arr.length-1))
+
+// namun jika menggunakan binary search. Binary search baru bisa digunakan secara efektif jika kita 
+// mempunyai array yang berurut.
+function findIndexBin(arr, target){
+    let left = 0
+    let right = arr.length-1
+
+    while(left <= right){
+        let mid = Math.floor((left + right)/2)
+        if(arr[mid] === target) return mid
+        else if(arr[mid] < target){left = mid+1}
+        else{right = mid-1}
+    }
+    return -1
+}
+
+console.log(findIndex([2, 8, 9, 10, 12, 14, 16], 12))
