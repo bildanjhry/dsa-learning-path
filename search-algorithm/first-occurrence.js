456f5c8aa1051ed298537d7cc9b7daf22e36a6fe
bildan <bildanjauhary@gmail.com>
2026-06-28 23:38:34 +0700

    docs: update typo comment for first occurrence algorithm

--- search-algorithm/first-occurrence.js
+++ search-algorithm/first-occurrence.js
@@ -1,7 +1,8 @@
-// First occurrence search algorithm
-// cara mencari kapan suatu value muncul untuk pertama kali.
-// program ini menggunakan pendekatan binary search O(log n) dan O(n) auxiliary space  
-// maka untuk menerapkan alogritma ini data harus terurut terlebih dahulu.
+ /*First occurrence search algorithm
+ cara mencari kapan suatu value muncul untuk pertama kali.
+ program ini menggunakan pendekatan binary search O(log n) dan O(1) auxiliary space   
+ maka untuk menerapkan alogritma ini data harus terurut terlebih dahulu. */
+
 function findFirstBinary(list, target){
     let low = 0, high = list.length-1, point = -1
     while(low <= high){
@@ -18,5 +19,4 @@ function findFirstBinary(list, target){
 
 
 const nums = [1, 2, 2, 2, 3, 3, 4, 4, 5];
-
 console.log(findFirstBinary(nums, 3));
